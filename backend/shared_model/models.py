ff5f105ea5e78f339631f7b03f4f4cc53f59e5cf
Harley <harleygit@gmail.com>
2026-01-31 07:10:18 +0800

    Updated user model naa nay fk sa emp

--- backend/shared_model/models.py
+++ backend/shared_model/models.py
@@ -1,45 +1,7 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
 
-class User(models.Model):
 
-    ROLE_CHOICES = (
-        ('EMPLOYEE', 'Employee'),
-        ('ADMIN', 'Admin'),
-        ('SUPER_ADMIN', 'Super Admin'),
-    )
-
-    user_id = models.AutoField(primary_key=True)
-    user_name = models.CharField(max_length=150, unique=True)
-    user_password = models.CharField(max_length=255)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='EMPLOYEE')
-    is_active = models.BooleanField(default=True)
-
-    # Temporary field (no FK yet)
-    emp_id = models.IntegerField(null=True, blank=True)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def set_password(self, raw_password):
-        self.user_password = make_password(raw_password)
-
-    def check_password(self, raw_password):
-        return check_password(raw_password, self.user_password)
-    
-    def save(self, *args, **kwargs):
-        if self.user_password and not self.user_password.startswith("pbkdf2_"):
-            self.set_password(self.user_password)
-        super().save(*args, **kwargs)
-
-
-    def __str__(self):
-        return f"{self.user_name} ({self.role})"
-
-    class Meta:
-        db_table = 'users'
-
-# Create your models here.
 class Department(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
@@ -106,7 +68,40 @@ class Employee(models.Model):
     shift = models.ForeignKey( Shift, on_delete=models.PROTECT,related_name="employees")
     department = models.ForeignKey( Department,on_delete=models.PROTECT,related_name="employees")
 
+class User(models.Model):
 
+    ROLE_CHOICES = (
+        ('EMPLOYEE', 'Employee'),
+        ('ADMIN', 'Admin'),
+        ('SUPER_ADMIN', 'Super Admin'),
+    )
+
+    user_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=150, unique=True)
+    user_password = models.CharField(max_length=255)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='EMPLOYEE')
+    is_active = models.BooleanField(default=True)
+    employee = models.OneToOneField( Employee, on_delete=models.CASCADE,related_name="user")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def set_password(self, raw_password):
+        self.user_password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.user_password)
+    
+    def save(self, *args, **kwargs):
+        if self.user_password and not self.user_password.startswith("pbkdf2_"):
+            self.set_password(self.user_password)
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return f"{self.user_name} ({self.role})"
+
+    class Meta:
+        db_table = 'users'
     
 
     

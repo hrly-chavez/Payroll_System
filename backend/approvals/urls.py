116be1004fbfb934251d4e25482350bc8e36ccea
Jin <jin@gmail.com>
2026-02-05 02:12:13 +0800

    sup ad - minor correction in approval urls

--- backend/approvals/urls.py
+++ backend/approvals/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HolidayListView, HolidayUpdateStatusView, PendingPayrollListView
+from .views import HolidayListView, HolidayUpdateStatusView
 urlpatterns = [
     path('superadmin/holiday/<int:pk>/status/', HolidayUpdateStatusView.as_view(), name='holiday-update-status'),
     path('superadmin/holidays/',HolidayListView.as_view(),name='holidays-list'),

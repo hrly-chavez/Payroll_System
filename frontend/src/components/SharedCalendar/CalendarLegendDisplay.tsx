5f29e1b7a7b4ef1f30b464143488910fbab67fbd
akmia <angelkhylaaboloc@gmail.com>
2026-02-18 07:00:04 +0800

    CALENDAR LEGEND MODIFIED

--- frontend/src/components/SharedCalendar/CalendarLegendDisplay.tsx
+++ frontend/src/components/SharedCalendar/CalendarLegendDisplay.tsx
@@ -3,44 +3,65 @@ import { HOLIDAY_LEGEND, PAYROLL_COLOR } from "./CalendarLegend";
 
 export default function CalendarLegendDisplay() {
   return (
-    <div className={styles.legendContainer}>
-      
-      {/* Payroll */}
-      <div className={styles.legendItem}>
-        <span
-          className={styles.colorDot}
-          style={{ background: PAYROLL_COLOR.bgColor }}
-        />
-        <span>Payroll Period</span>
+    <div className={styles.legendWrapper}>
+
+      {/* Payroll on top */}
+      <div className={styles.payrollRow}>
+        <div className={styles.legendItem}>
+          <span
+            className={styles.colorDot}
+            style={{ background: PAYROLL_COLOR.bgColor }}
+          />
+          <span>Payroll Period</span>
+        </div>
       </div>
 
-      {/* PH Regular */}
-      <div className={styles.legendItem}>
-        <span
-          className={styles.colorDot}
-          style={{ background: HOLIDAY_LEGEND.PH.Regular.bgColor }}
-        />
-        <span>PH Regular Holiday</span>
-      </div>
+      {/* Three column layout */}
+      <div className={styles.legendGrid}>
 
-      {/* US Regular */}
-      <div className={styles.legendItem}>
-        <span
-          className={styles.colorDot}
-          style={{ background: HOLIDAY_LEGEND.US.Regular.bgColor }}
-        />
-        <span>US Regular Holiday</span>
-      </div>
+        {/* PH */}
+        <div className={styles.legendGroup}>
+          <div className={styles.legendTitle}>PH Holidays</div>
+          {Object.entries(HOLIDAY_LEGEND.PH).map(([type, config]) => (
+            <div key={`PH-${type}`} className={styles.legendItem}>
+              <span
+                className={styles.colorDot}
+                style={{ background: config.bgColor }}
+              />
+              <span>{type}</span>
+            </div>
+          ))}
+        </div>
 
-      {/* Company Holiday */}
-      <div className={styles.legendItem}>
-        <span
-          className={styles.colorDot}
-          style={{ background: HOLIDAY_LEGEND.COMPANY["Company Holiday"].bgColor }}
-        />
-        <span>Company Holiday</span>
-      </div>
+        {/* US */}
+        <div className={styles.legendGroup}>
+          <div className={styles.legendTitle}>US Holidays</div>
+          {Object.entries(HOLIDAY_LEGEND.US).map(([type, config]) => (
+            <div key={`US-${type}`} className={styles.legendItem}>
+              <span
+                className={styles.colorDot}
+                style={{ background: config.bgColor }}
+              />
+              <span>{type}</span>
+            </div>
+          ))}
+        </div>
 
+        {/* COMPANY */}
+        <div className={styles.legendGroup}>
+          <div className={styles.legendTitle}>Company Holidays</div>
+          {Object.entries(HOLIDAY_LEGEND.COMPANY).map(([type, config]) => (
+            <div key={`COMPANY-${type}`} className={styles.legendItem}>
+              <span
+                className={styles.colorDot}
+                style={{ background: config.bgColor }}
+              />
+              <span>{type}</span>
+            </div>
+          ))}
+        </div>
+
+      </div>
     </div>
   );
 }

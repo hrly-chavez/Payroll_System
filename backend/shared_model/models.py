8ef2fe77bee5bc5bcb4c1dfc4fa141ea817d0093
Harley <harleygit@gmail.com>
2026-03-10 04:25:54 +0800

    added default in cutoff_type

--- backend/shared_model/models.py
+++ backend/shared_model/models.py
@@ -730,7 +730,7 @@ class Payroll_Period(models.Model):
     code = models.CharField(max_length=100,unique=True)
     start_date = models.DateField()
     end_date = models.DateField()
-    cutoff_type = models.CharField(max_length=10,choices=period_type_choices)
+    cutoff_type = models.CharField(max_length=10,choices=period_type_choices,default="FIRST")
     pay_date = models.DateField(null=True, blank=True)
     color = models.CharField(max_length=20, default="#ff4d4f")
     status = models.CharField(max_length=20, choices=period_status_choices, default="Open")

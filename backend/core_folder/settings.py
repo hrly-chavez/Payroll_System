73d7031343483d20deba3f70e2ab6be80bb2fc9f
Harley <harleygit@gmail.com>
2026-05-20 01:54:45 +0800

    "Fix Django admin static URL"

--- backend/core_folder/settings.py
+++ backend/core_folder/settings.py
@@ -222,7 +222,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/django-static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
